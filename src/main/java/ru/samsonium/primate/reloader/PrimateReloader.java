605390780d6c6fb8d2bdeb4a9dee2cbefd9b90ec
Samsonium <tdesu@vk.com>
2024-01-11 11:48:08 +0300

    Add modifies get method

--- src/main/java/ru/samsonium/primate/reloader/PrimateReloader.java
+++ src/main/java/ru/samsonium/primate/reloader/PrimateReloader.java
@@ -1,8 +1,15 @@
 package ru.samsonium.primate.reloader;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
 public final class PrimateReloader extends JavaPlugin {
+    private Map<String, Long> modifies;
 
     @Override
     public void onEnable() {
@@ -13,5 +20,37 @@ public final class PrimateReloader extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    /**
+     * Get map of files and it's last modified time
+     * @return Map of files with last modified time
+     * @throws Exception if got an error on reading list files
+     */
+    public Map<String, Long> getPlugins() throws Exception {
+        File pluginsFolder = getServer().getPluginsFolder();
+        File[] fileList = pluginsFolder.listFiles();
+        if (fileList == null)
+            throw new Exception("Got an error on list files");
+
+        Map<String, Long> result = new HashMap<>();
+        for (File pluginDir : fileList) {
+            if (!pluginDir.isDirectory()) continue;
+
+            String name = pluginDir.getName();
+            Plugin plugin = getServer().getPluginManager().getPlugin(name);
+            if (plugin == null) continue;
+
+            File pluginJar = new File(plugin.getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
+            result.put(pluginDir.getName(), pluginJar.lastModified());
+        }
+
+        return result;
+    }
+
+    /**
+     * Get base plugin class instance
+     * @return PrimateReloader instance
+     */
+    public static PrimateReloader get() {
+        return getPlugin(PrimateReloader.class);
     }
 }
